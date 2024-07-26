d6cc29672c474bc0db1035981a87deab1c414258
unknown <gabriel.almeida.2004@gmail.com>
2024-07-25 21:30:33 -0300

    first commit

---
+++ src/main/java/com/start/produtos/model/RespostaModel.java
@@ -0,0 +1,13 @@
+package com.start.produtos.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+public class RespostaModel {
+
+    private String mensagem;
+}
